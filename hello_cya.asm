3bc321f4701c71483c881733be335e9198bdb4db
realabdo <abdullah.rm.2020@gmail.com>
2023-10-11 15:30:06 +0300

    new

--- hello_cya.asm
+++ hello_cya.asm
@@ -2,7 +2,7 @@
 [org 0x7c00] ; tell the assembler where to put this  code ( location counter ) 
 
 mov ah, 0x0e ; indicate teletype scrolling bios routine mode 
-mov bx, 0x1fb6; using general purpose bx register as a parameter. 
+mov bx, hello_msg ; using general purpose bx register as a parameter. 
 call print_string ; calling the function 
 
 mov bx, goodbye_msg ;again we use bx s parameter
@@ -16,7 +16,7 @@ goodbye_msg:
 db 'good bye',0
 %include  "print_string.asm" ; it's like importing external function
 ; regular boot_sector stuff
-print_hex:
+
 
 times 510-($-$$) db 0 ;zeros padding to make 521 byets.
 
