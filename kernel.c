6f0570a17b5ad8fe5f3131a709efba779dde42e2
realabdo <abdullah.rm.2020@gmail.com>
2023-10-14 14:03:27 +0300

    new kernel

---
+++ kernel.c
@@ -0,0 +1,6 @@
+void main(){
+    // create a pointer to a char in the video memeroy
+    char* video_memory = (char*) 0xb8000;
+    // store char x, at the top-left. because video memory is sequential
+    *video_memory = 'X';
+}
