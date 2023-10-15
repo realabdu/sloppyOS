ef4a41ea696c3592765c0016cca293a34065c8b2
Ubuntu <ubuntu@ip-172-31-20-121.eu-central-1.compute.internal>
2023-10-15 08:47:12 +0000

    new kernel

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,34 +1,32 @@
-;
-; boot sctor program that loops forever. 
-;
+; this is a boot sector, that helps booting a kernel in 32-bit protected mode.
+[org 0x7c00]
 
+KERNEL_OFFSET equ 0x1000 ;memory offset of which we will load our kernel
 
+mov [BOOT_DRIVE], dl 
 
-mov ah, 0x0e ; indicate teletype scrolling bios routine mode 
+mov bp, 0x9000 ;setup the stack.
+mov sp,bp 
 
-mov bp, 0x8000 ; this will set the base of the stackk, a little above where the BIOS 
-mov sp, bp ; this should mean that we load our boot sector, so it wont overwrite ??
+mov bx, MSG_SWITCH_REALMODE
+call load_kernel
 
-push 'A' ; we push some characters, on the stack for later retreival, these are 16-bit values. as our stack is 16-bit stack
-push 'B' 
-push 'C'
+call switch_to_pm
 
+jmp $
 
-pop bx ; pop to px
-mov al, bl ; copy bl to al for printing
-int 0x10
+;routines
 
-pop bx ; pop the next value 
-mov al, bl ; copy bl to al for printing
-int 0x10
-; now we want to prove that our stack actually moves downward, so we are going to fetch the value of the base of the stack
+% include "print_string.asm"
+% include "gdt.asm"
+% include "print_string_pm.asm"
+% include "switch_to_pm.asm"
 
-mov al, [0x7ffe]
-int 0x10
+[bits 16]
 
-jmp $ ;jump to the current address ( this is the new infinte loop
-;loop:                           define a label loop, and we will jump back to it forever ( infinite loop )
-;    jmp loop
-times 510-($-$$) db 0           ;tell the assembler to pad enough zeros (db 0), of 510 bytes to bring us to the last two 
+load_kernel:
+mov bx, MSG_LOAD_KERNEL
+call print_string
 
-dw 0xaa55                       ;magic number, that indicates this is the boot sector ( one word 16-bits )
+mov bx, KERNEL_OFFSET
+mov dh, 15
